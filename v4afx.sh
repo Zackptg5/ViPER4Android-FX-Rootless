bd7c72c803eb4aaf40f22ef3e4afc1ba6c6f5ec1
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-10-21 14:57:08 -0400

    update ak3

--- v4afx.sh
+++ v4afx.sh
@@ -2,10 +2,5 @@
 (until [ "$(getprop sys.boot_completed)" == "1" ]; do
   sleep 1
 done
-am start -a android.intent.action.MAIN -n com.pittvandewitt.viperfx/com.audlabs.viperfx.main.MainActivity
-until [ "$(pidof com.pittvandewitt.viperfx)" ]; do
-  sleep 3
-done
-killall com.pittvandewitt.viperfx
-killall audioserver
-killall mediaserver)&
+killall -q audioserver
+killall -q mediaserver)&
